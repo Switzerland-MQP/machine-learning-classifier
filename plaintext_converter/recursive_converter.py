9b9d497ac279685b312adb600aa60deb8cdf7b36
Harry Sadoyan <harrysadoyan@gmail.com>
2018-08-17 09:43:54 -0400

    Recursive version works

--- plaintext_converter/recursive_converter.py
+++ plaintext_converter/recursive_converter.py
@@ -1,19 +1,38 @@
 import os
 import sys
 
+from to_plaintext import to_plaintext
 
 if __name__ == "__main__":
     source = sys.argv[1]
     dest = sys.argv[2]
+    whitelist = [
+        ".csv", ".doc", ".docx", ".eml",
+        ".epub", ".gif", ".htm", ".html",
+        ".jpeg", ".jpg", ".json", ".log",
+        ".mp3", ".msg", ".odt", ".ogg", ".pdf",
+        ".png", ".pptx", ".ps", ".psv", ".rtf",
+        ".tff", ".tif", ".tiff", ".tsv", ".txt",
+        ".wav", ".xls", ".xlsx"
+    ]
     for root, dirs, files in os.walk(source):
         path = root.split(os.sep)
 
         for file in files:
             print("File: ", file)
-            # Call ToPlaintext function here
+
+            _, file_extension = os.path.splitext(file)
+            if file_extension not in whitelist:
+                continue
+
+            text = to_plaintext(root + "/" + file)
             destFileName = file + ".txt"
             destFileName = "{}/{}.txt".format(dest, file)
             print("Dest File Name: ", destFileName)
 
-            destFile = open(destFileName, "w")
+            destFile = open(destFileName, "wb")
+            destFile.write(text)
             destFile.close()
+
+
+
