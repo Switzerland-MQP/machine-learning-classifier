e112ca0271695a3e3d4c361817b1fea845a04608
Griffin Bishop <grbishop@wpi.edu>
2018-08-29 12:50:40 +0200

    Rename new file

--- parser/label_parser.py
+++ parser/label_parser.py
@@ -4,103 +4,104 @@ personal_categories = ['name','id-number', 'location', 'online-id', 'dob', 'phon
 sensitive_categories = ['criminal', 'origin', 'health', 'religion', 'political', 'philosophical', 'unions', 'sex-life', 'sex-orientation', 'biometric']
 all_categories = personal_categories + sensitive_categories
 
-def get_index_of_nth_line(string, n):
-	n -= 1
-	i = 0
-	counter = 0
-	while n > 0:
-		if string[counter] == '\n':
-			n -= 1
-			counter += 1
-			continue
-		i += 1
-		counter += 1	
-	return i
 
-def pos_splice(raw_document, start_pos, end_pos):
-	start_line, start_index = start_pos
-	end_line, end_index = end_pos	
+class Tag:
+	def __init__(self, name):
+		self.name = name
+		self.children = []
+		self.is_open = True
+	
+	def __repr__(self):
+		return self.name + "(" + str(self.children) + ")"
 
-	start = get_index_of_nth_line(raw_document, start_line) + start_index
-	end = get_index_of_nth_line(raw_document, end_line) + end_index
+	def name(self, name):
+		self.name = name
 
-	document = raw_document
-	#document = ''.join(raw_document.split('\n')) #toggle comment to count newline characters
-	return document[start:end]
-
-
-def pos_slice_wrapper(raw_document, start_pos, end_pos):
-	print("Slicing:", start_pos, " --> ", end_pos, "\n of doc: \n", raw_document, "||")
-	print(pos_splice(raw_document, start_pos, end_pos))
-
+	def close(self):
+		self.is_open = False
 
 class LabelParser(HTMLParser):
 	def __init__(self):
 		super().__init__()
 		self.raw_document = ''
-		self.previous_pos = (0,0)
 		self.document = []
-		self.status = 'text'
-		self.current_labels = []
+		self.verbosity = 0
 
-	def feed(self, raw_document):
+	def parse(self, raw_document):
 		self.raw_document = raw_document
 		super().feed(raw_document)
-	
-	def tag_is_valid(self, delimited_tag):
-		tags = delimited_tag.split('_')
-		return all([tag in all_categories for tag in tags])
+		return self.document
 
-	def label_lines(self, start_pos, end_pos, labels):
-		pass
-
-	def handle_starttag(self, tag, attrs):
-		if len(attrs) > 0:
-			raise Exception("Error: tag has spaces in name")
-	
-		print(self.getpos(), "Encountered a start tag - Current status: ", self.status, " | : ", tag)
+	def handle_starttag(self, tag_name, attrs):
+		if self.verbosity > 1:
+			print(self.getpos(), "Encountered a start tag - Current status| : ", tag_name)
 		
-		if self.tag_is_valid(tag):
-			if self.status != 'text':
-				raise Exception("Error: open tag " + tag + " encountered without closing previous tag")
+		if len(attrs) > 0:
+			raise Exception("Parse Error: tag has spaces in name")
+	
+		self.handle_starttag_helper(self.document, tag_name)	
 
-			self.label_lines(self.previous_pos, self.getpos(), ['not personal'])
-			self.current_labels = tag.split('_')
-			self.status = 'label'
-		elif tag == 'data':
-			if self.status != 'label':
-				raise Exception("Error: open data tag encounted outside of a label")
-			if len(self.current_labels) < 1:
-				raise Exception("Error: open data tag encountered but no labels were available to apply")			
+	
+	def handle_starttag_helper(self, on_list, tag_name):
+		if len(on_list) < 1:
+			on_list.append(Tag(tag_name))
+			return		
+		last_expression = on_list[-1]
+	
+		if type(last_expression) is str:
+			on_list.append(Tag(tag_name))
+		elif type(last_expression) is Tag:
+			if not last_expression.is_open:
+				on_list.append(Tag(tag_name))
+			else: #last_expression is open
+				self.handle_starttag_helper(last_expression.children, tag_name)
 
-			self.label_lines(self.previous_pos, self.getpos(), [label + '-context' for label in self.current_labels])
-			self.status = 'data'
-		else:
-			raise Exception("Error: " + tag + " is not a valid category")
+
+	def handle_endtag(self, tag_name):
+		if self.verbosity > 1:
+			print(self.getpos(), "Encountered an end tag| :", tag_name)
+		
+		self.handle_endtag_helper(self.document, tag_name)
+
+	
+	def handle_endtag_helper(self, on_list, tag_name):
+		if len(on_list) < 1:
+			raise Exception("Parse Error: close tag mismatch:", tag_name)
+		last_expression = on_list[-1]
+
+		if type(last_expression) is str:
+			raise Exception("Parse Error: encountered close tag ", tag_name, " without a matching open tag")
+		elif type(last_expression) is Tag:
+			if not last_expression.is_open:
+				raise Exception("Parse Error: encountered close tag ", tag_name, " without an open tag")
+			else: # last_expression is open
+				if not last_expression.name == tag_name:
+					self.handle_endtag_helper(last_expression.children, tag_name)	
+				else:
+					last_expression.close()
 
 
-	def handle_endtag(self, tag):
-		print(self.getpos(), "Encountered an end tag - Status:", self.status, "| :", tag)
-		if self.tag_is_valid(tag):
-			if self.status != 'label':
-				raise Exception("Error: Close tag ", tag, " encountered without a matching open tag")
-			if self.current_labels != tag.split('_'):
-				raise Exception("Error: Close tag ", tag, " does not match open tag ", self.current_labels.join('_'))
+	def handle_data(self, text):
+		if self.verbosity > 1:
+			print(self.getpos(), "Encountered some text | :", text)
+	
+		self.handle_data_helper(self.document, text)
 
-			self.label_lines(self.previous_pos, self.getpos(), [label + '-context' for label in self.current_labels])
-			self.current_labels = []
-			self.status = 'text'
-		elif tag == 'data':
-			if self.status != 'data':
-				raise Exception("Error: </data> close tag encountered without a matching open data tag")
-			
-			self.label_lines(self.previous_pos, self.getpos(), self.current_labels)
-			self.status = 'label'
-		else:
-			raise Exception("Error: close tag " + tag + " is not a valid tag")
-
-	def handle_data(self, data):
-		print(self.getpos(), "Encountered some data - Status:", self.status, "| :", data)
+	
+	def handle_data_helper(self, on_list, text):
+		if len(on_list) < 1:
+			on_list.append(text)
+			return
+		last_expression = on_list[-1]
+		
+		if type(last_expression) is str:
+			raise Exception("Parse internal Error: encountered text directly after text")
+		elif type(last_expression) is Tag:
+			if not last_expression.is_open:
+				on_list.append(text)
+			else:
+				#last expression is open
+				self.handle_data_helper(last_expression.children, text)
 
 
 if __name__ == "__main__":
@@ -108,7 +109,10 @@ if __name__ == "__main__":
 	document = """ <online-id_phone_name>Your contact:</online-id_phone_name>\n
 		blah \nblah <name> my name is <data> griffin</data></name> \n
 		and blah <name> my uncle's name is<data> dave</data></name> blah \n blah"""
-	parser.feed(document)
+	ast = parser.parse(document)
+
+	print("Input :\n ", document)
+	print(ast)
 
 	#document = """0123456789\n0123456789\n"""
 	#pos_slice_wrapper(document, (1, 0), (1, 5))
