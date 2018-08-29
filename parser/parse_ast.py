d1aa95579331a6d372f9e04d91d63012303e0f35
Griffin Bishop <grbishop@wpi.edu>
2018-08-29 12:49:20 +0200

    Parser is complete

--- parser/parse_ast.py
+++ parser/parse_ast.py
@@ -10,12 +10,12 @@ class Tag:
 		self.name = name
 		self.children = []
 		self.is_open = True
+	
+	def __repr__(self):
+		return self.name + "(" + str(self.children) + ")"
 
 	def name(self, name):
 		self.name = name
-
-	def add_child(self, child)
-		self.children = self.children + [child]
 
 	def close(self):
 		self.is_open = False
@@ -25,38 +25,49 @@ class LabelParser(HTMLParser):
 		super().__init__()
 		self.raw_document = ''
 		self.document = []
-		self.status = 'text'
+		self.verbosity = 0
 
-	def feed(self, raw_document):
+	def parse(self, raw_document):
 		self.raw_document = raw_document
 		super().feed(raw_document)
-	
+		return self.document
+
 	def handle_starttag(self, tag_name, attrs):
-		print(self.getpos(), "Encountered a start tag - Current status| : ", tag_name)
+		if self.verbosity > 1:
+			print(self.getpos(), "Encountered a start tag - Current status| : ", tag_name)
 		
 		if len(attrs) > 0:
 			raise Exception("Parse Error: tag has spaces in name")
-		
-		if len(self.document) < 1:
-			self.document.append(Tag(tag_name))
+	
+		self.handle_starttag_helper(self.document, tag_name)	
+
+	
+	def handle_starttag_helper(self, on_list, tag_name):
+		if len(on_list) < 1:
+			on_list.append(Tag(tag_name))
 			return		
-		last_expression = self.document[-1]
+		last_expression = on_list[-1]
 	
 		if type(last_expression) is str:
-			self.document.append(Tag(tag_name))
+			on_list.append(Tag(tag_name))
 		elif type(last_expression) is Tag:
 			if not last_expression.is_open:
-				self.document.append(Tag(tag_name))
+				on_list.append(Tag(tag_name))
 			else: #last_expression is open
-				last_expression.add_child(Tag(tag_name))
-				
+				self.handle_starttag_helper(last_expression.children, tag_name)
+
 
 	def handle_endtag(self, tag_name):
-		print(self.getpos(), "Encountered an end tag| :", tag_name)
+		if self.verbosity > 1:
+			print(self.getpos(), "Encountered an end tag| :", tag_name)
 		
-		if len(self.document) < 1:
-			raise Exception("Parse Error: encountered close tag at start of document")
-		last_expression = self.document[-1]
+		self.handle_endtag_helper(self.document, tag_name)
+
+	
+	def handle_endtag_helper(self, on_list, tag_name):
+		if len(on_list) < 1:
+			raise Exception("Parse Error: close tag mismatch:", tag_name)
+		last_expression = on_list[-1]
 
 		if type(last_expression) is str:
 			raise Exception("Parse Error: encountered close tag ", tag_name, " without a matching open tag")
@@ -65,39 +76,46 @@ class LabelParser(HTMLParser):
 				raise Exception("Parse Error: encountered close tag ", tag_name, " without an open tag")
 			else: # last_expression is open
 				if not last_expression.name == tag_name:
-					raise Exception("Parse Error: mismatch close tag ", tag_name, " for open tag ", last_expression.name)
+					self.handle_endtag_helper(last_expression.children, tag_name)	
 				else:
 					last_expression.close()
 
-	def handle_data(self, text):
-		print(self.getpos(), "Encountered some text | :", text)
 
-		if len(self.document) < 1:
-			self.document.append(text)
+	def handle_data(self, text):
+		if self.verbosity > 1:
+			print(self.getpos(), "Encountered some text | :", text)
+	
+		self.handle_data_helper(self.document, text)
+
+	
+	def handle_data_helper(self, on_list, text):
+		if len(on_list) < 1:
+			on_list.append(text)
 			return
-		last_expression = self.document[-1]
+		last_expression = on_list[-1]
 		
 		if type(last_expression) is str:
 			raise Exception("Parse internal Error: encountered text directly after text")
 		elif type(last_expression) is Tag:
 			if not last_expression.is_open:
-				self.document.append(text)
+				on_list.append(text)
 			else:
 				#last expression is open
-				last_expression.add_child(text)
+				self.handle_data_helper(last_expression.children, text)
 
-	def handle_data_helper(self, on_list, text):
-		pass
 
 if __name__ == "__main__":
-	#parser = LabelParser()
-	#document = """ <online-id_phone_name>Your contact:</online-id_phone_name>\n
-	#	blah \nblah <name> my name is <data> griffin</data></name> \n
-	#	and blah <name> my uncle's name is<data> dave</data></name> blah \n blah"""
-	#parser.feed(document)
+	parser = LabelParser()
+	document = """ <online-id_phone_name>Your contact:</online-id_phone_name>\n
+		blah \nblah <name> my name is <data> griffin</data></name> \n
+		and blah <name> my uncle's name is<data> dave</data></name> blah \n blah"""
+	ast = parser.parse(document)
 
-	document = """0123456789\n0123456789\n"""
-	pos_slice_wrapper(document, (1, 0), (1, 5))
+	print("Input :\n ", document)
+	print(ast)
+
+	#document = """0123456789\n0123456789\n"""
+	#pos_slice_wrapper(document, (1, 0), (1, 5))
 
 
 
