d3f09f3cd2a5e3e56a614d5cfc3389310686fc2c
Griffin Bishop <grbishop@wpi.edu>
2018-09-04 12:33:56 +0200

    Fix count_tagged_lines.py

--- count_tagged_lines.py
+++ count_tagged_lines.py
@@ -4,10 +4,10 @@ from os import listdir
 from os.path import isfile, join
 #  from parser.to_lines import Line
 
-parent_dir = '../TEXTDATA/LINE_BY_LINE'
+parent_dir = './TEXTDATA'
 personal_dir = parent_dir + '/PERSONAL_DATA/html-tagged'
 sensitive_dir = parent_dir + '/SENSITIVE_DATA/html-tagged'
-non_personal_dir = './NON_PERSONAL_DATA'
+non_personal_dir = parent_dir + '/NON_PERSONAL_DATA'
 
 
 def get_files_in_dir(path):
@@ -47,17 +47,17 @@ def count_lines(files):
 	for category in category_totals.keys():
 		print(category + " => " + str( category_totals[category] ))
 
-
-files = get_files_in_dir(non_personal_dir)
-#  files = get_files_in_dir(personal_dir)
-#  files = files + get_files_in_dir(sensitive_dir)
+print("Files we HTML tagged: ")
+files = get_files_in_dir(personal_dir)
+files = files + get_files_in_dir(sensitive_dir)
 
 count_lines(files)
 
-#print(" ================================================" )
-#files = get_files_in_dir(parent_dir + "/PERSONAL_DATA/")
-#files = files + get_files_in_dir(parent_dir + "/SENSITIVE_DATA/")
+print(" ================================================" )
+print("Files we didn't tag yet:")
+files = get_files_in_dir(parent_dir + "/PERSONAL_DATA/")
+files = files + get_files_in_dir(parent_dir + "/SENSITIVE_DATA/")
 
-#count_lines(files)
+count_lines(files)
 
 		
