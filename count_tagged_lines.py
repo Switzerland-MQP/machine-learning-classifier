8c774e391d966653680f77ee1a5ed810a1d2aea0
leogrande <leosgrande1@gmail.com>
2018-09-04 21:16:44 -0400

    merge stuff

--- count_tagged_lines.py
+++ count_tagged_lines.py
@@ -4,9 +4,11 @@ from os import listdir
 from os.path import isfile, join
 #  from parser.to_lines import Line
 
-parent_dir = '../TEXTDATA/LINE_BY_LINE'
-personal_dir = parent_dir + '/PERSONAL_DATA/html-tagged'
-sensitive_dir = parent_dir + '/SENSITIVE_DATA/html-tagged'
+parent_dir = '.'
+#  personal_dir = parent_dir + '/PERSONAL_DATA/html-tagged'
+#  sensitive_dir = parent_dir + '/SENSITIVE_DATA/html-tagged'
+non_personal_dir = './NON_PERSONAL_DATA'
+
 
 def get_files_in_dir(path):
 	return [join(path, f) for f in listdir(path) if isfile(join(path, f))]
@@ -22,10 +24,10 @@ def count_lines(files):
 		try:
 			lines = run_parser(f)
 			files_processed += 1
-		except Exception as e:		
+		except Exception as e:
 			error_files += 1
 			print(f + " --> " + str(e))
-			continue			
+			continue
 
 		#print("File " + f + " has " + str(len(lines)) + " lines")
 
@@ -46,15 +48,14 @@ def count_lines(files):
 		print(category + " => " + str( category_totals[category] ))
 
 
-files = get_files_in_dir(personal_dir)
-files = files + get_files_in_dir(sensitive_dir)
-
-count_lines(files)
-
-#print(" ================================================" )
-#files = get_files_in_dir(parent_dir + "/PERSONAL_DATA/")
-#files = files + get_files_in_dir(parent_dir + "/SENSITIVE_DATA/")
+#files = get_files_in_dir(personal_dir)
+#files = files + get_files_in_dir(sensitive_dir)
 
 #count_lines(files)
 
-		
+print(" ================================================" )
+#  files = get_files_in_dir(parent_dir + "/PERSONAL_DATA/")
+#  files = files + get_files_in_dir(parent_dir + "/SENSITIVE_DATA/")
+files = get_files_in_dir(non_personal_dir)
+
+count_lines(files)
