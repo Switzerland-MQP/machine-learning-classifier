aafdd47e6f455a04786df0cf106642b3bd8f0f51
Griffin Bishop <grbishop@wpi.edu>
2018-09-11 12:48:28 +0200

    PCA viz on document level

--- pca_viz.py
+++ pca_viz.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
 
 import utils
 
+"""
 documents = utils.load_dirs_custom([
 	'./TAGGED_DATA/SENSITIVE_DATA/html-tagged',
 	'./TAGGED_DATA/PERSONAL_DATA/html-tagged',
@@ -20,13 +22,20 @@ doc_train, doc_test = utils.document_test_train_split(
 
 x_train, y_train = utils.convert_docs_to_lines(doc_train)
 x_test, y_test = utils.convert_docs_to_lines(doc_test)
+"""
+
+documents = load_files('./TEXTDATA/')
+x_train, x_test, y_train, y_test = train_test_split(
+	documents.data, documents.target, test_size=0
+)
+
 
 reducer = Pipeline([('count', CountVectorizer()),
 										('tfidf', TfidfTransformer()),
 										('pca', TruncatedSVD(n_components=2))])
-
 x_train_reduced = reducer.fit_transform(x_train)
-x_test_reduced = reducer.transform(x_test)
 
-plt.scatter(x_train_reduced[:,0], x_train_reduced[:,1], c=y_train)
+colors = np.where(y_train == 0, 'green', 'red')
+
+plt.scatter(x_train_reduced[:,0], x_train_reduced[:,1], c=colors)
 plt.show()
