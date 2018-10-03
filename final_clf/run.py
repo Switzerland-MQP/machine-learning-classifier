d93a59beea26ce446853b849c22d4cc0b0035653
Griffin Bishop <grbishop@wpi.edu>
2018-10-03 14:04:20 +0200

    run.py convert to for loop

--- final_clf/run.py
+++ final_clf/run.py
@@ -1,19 +1,22 @@
 ###############################################
-#  Final classifier pipeline                  #
-#  Authors: Griffin Bishop, Sam Pridotkas,    #
-#  Harry Sadoyan, Leo Grande                  #
+## Final classifier pipeline                 ##
+## Authors: Griffin Bishop, Sam Pridotkas,   ##
+## Harry Sadoyan, Leo Grande                 ##
 ###############################################
 
 # Pipeline steps:
 #
 # 1. Convert directory of documents (pdfs, excel, word, text) into text files
-# 
 #
 #
 #
+#
+
 
 ### Begin imports ###
 
+import time
+start = time.time()
 from sklearn.datasets import load_files
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -26,72 +29,72 @@ import numpy as np
 from keras.utils import np_utils
 from keras.models import model_from_json
 import utils
-import time
-start = time.time()
-
+import pickle
 ### End imports ###
 
 
 
 
+
 ### Begin models ###
-CATEGORY_F2_SCORE_CUTOFF = 0.5
-
-
 def load_keras_model(json_path, weights_path):
-    json_file = open(json_path, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights(weights_path)
-    return loaded_model
+	json_file = open(json_path, 'r')
+	loaded_model_json = json_file.read()
+	json_file.close()
+	loaded_model = model_from_json(loaded_model_json)
+	loaded_model.load_weights(weights_path)
+	return loaded_model
+
+def confidence(predicted_vec):
+	predicted_vec = np.array(predicted_vec)
+	norm = np.linalg.norm(np.sum(predicted_vec))
+	divided = (predicted_vec.T/norm).T
+	stdev = np.std(divided)
+	return stdev
 
 
-def run_model(filepath):
-    print("Beginning document preprocessing for document level classification")
-    documents = load_files(filepath, shuffle=False)
-    x = documents.data
-    preprocessing = Pipeline([('count', CountVectorizer()),
-                              ('tfidf', TfidfTransformer()),
-                              ('pca', TruncatedSVD(n_components=430))])
-    preprocessing.fit(x)
-    x = preprocessing.transform(x)
-    print("Finished document preprocessing - document level classifcation - {} elapsed".format(time.time()-start))
+print("Loading models from disk...")
+preprocessing = pickle.load(open("./pipeline/models/preprocessing.pickle", "rb"))
+document_clf = load_keras_model("./pipeline/models/document-clf/model.json",
+																"./pipeline/models/document-clf/model.h5")
+category_clf = load_keras_model("./pipeline/models/category-clf/model.json",
+																"./pipeline/models/category-clf/model.h5")
+#TODO: load line clf
+print("Done loading models from disk.")
+
+import ipdb
 
 
+loaded_documents = load_files('./documents/text_documents', shuffle=False)
 
-    ##########
-    print("Loading document classifier model from disk")
-    document_clf = load_keras_model("./pipeline/models/document-clf/model.json",
-                                    "./pipeline/models/document-clf/model.h5")
-    predicted_vec = document_clf.predict(x)
-    predicted = np.argmax(predicted_vec, axis=1)
+# TODO: line preprocessing and prediction
+for i in range(len(loaded_documents.data)):
+	path = loaded_documents.filenames[i]
+	text = loaded_documents.data[i]
+	text_pca = preprocessing.transform([text])
 
-    print("Done predicting document classifications")
+	#Predict document class
+	predicted_vec = document_clf.predict(text_pca)
+	predicted_class = np.argmax(predicted_vec, axis=1)
 
+	#Predict individual categories
+	predicted_categories = category_clf.predict([text_pca])[0]
+	high_probability_categories = []
+	for i in range(22):
+		category = utils.all_categories_dict[i+1]
+		cutoff = 0.002 # TODO: make this cutoff programmatic
+										# We're going to have to just pick a number for each category
+		if predicted_categories[i+1] > 0.002:
+			high_probability_categories.append(category)
 
-    ##########
-    print("Loading individual category classifer model from disk")
-    category_clf = load_keras_model("./pipeline/models/category-clf/model.json",
-                                    "./pipeline/models/category-clf/model.h5")
-    predicted = category_clf.predict(x)
+		# Could also just take the highest 5 categories
 
-    for i in range(22):
-        category = utils.all_categories_dict[i+1]
-        predicted_category = predicted[:,i+1]
-        y_category = y_test[:,i+1]
+	# TODO: Line prediction
 
-        cutoff = 0.002 # TODO: make this cutoff programmatic 
+	print(f"File path: {path}")
+	print(f"Predicted class: {predicted_class} with confidence {confidence(predicted_vec)}")
+	print(f"High probability categories: {high_probability_categories}")
 
-        predicted_category = np.where(predicted_category > cutoff, 1, 0)
+	# TODO: put original file in new directory
 
-    # TODO: assign highest probability categories to each document
-
-
-
-    #########
-    print("Loading line classifier model from disk")
-
-
-
-
+	ipdb.set_trace()
